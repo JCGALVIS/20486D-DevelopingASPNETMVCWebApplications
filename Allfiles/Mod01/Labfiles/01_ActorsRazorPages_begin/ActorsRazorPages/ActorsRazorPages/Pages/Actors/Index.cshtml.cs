75176346be85e2986550220d3edeca596e199eae
Juan Carlos Galvis Rodriguez <carlos.galvis@sinco.com.co>
2019-04-12 12:43:57 -0500

    Primer laboratorio.

---
+++ Allfiles/Mod01/Labfiles/01_ActorsRazorPages_begin/ActorsRazorPages/ActorsRazorPages/Pages/Actors/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ActorsRazorPages.Models;
+
+namespace ActorsRazorPages.Pages.Actors
+{
+    public class IndexModel : PageModel
+    {
+        private IData _Data;
+        public IList<Actor> Actors { get; set; }
+
+        public IndexModel(IData data) {
+            this._Data = data;
+        }
+
+        public void OnGet()
+        {
+            Actors = this._Data.ActorsInitializeData();
+        }
+    }
+}
---
+++ Allfiles/Mod01/Labfiles/02_CakeStoreApi_begin/CakeStoreApi/CakeStoreApi/Controllers/CakeStoreApiController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CakeStoreApi.Models;
+
+namespace CakeStoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CakeStoreApiController : ControllerBase
+    {
+        private IData _data;
+
+        public CakeStoreApiController(IData data) {
+            this._data = data;
+        }
+
+        [HttpGet("/api/CakeStore")]
+        public ActionResult<List<CakeStore>> GetAll()
+        {
+            return _data.CakesInitializeData();
+        }
+
+        [HttpGet("/api/CakeStore/{id}")]
+        public ActionResult<CakeStore> GetById(int? Id) {
+            var item = this._data.GetCakeById(Id);
+
+            if (item == null) {
+                return NotFound();
+            }
+
+            return new ObjectResult(item);
+        }
+    }
+}
